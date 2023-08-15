0f17caf59593c52cf09bec12de0632ff485bd1df
Ekaterina Khoroshilova <ek.khorosh@gmail.com>
2023-08-15 16:18:29 -0700

    feature #3

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import HornedBeast from './HornedBeast';
 import hornedBeastValues from '../assets/data.json';
 
+
+
 class Gallery extends React.Component {
  
-
   render() {
     return (
-    <div>
-        {hornedBeastValues.map((beast, idx) => <HornedBeast key={idx} title={beast.title} image_url={beast.image_url} description={beast.description} />)}
-     
-    </div>
+        <section>
+          {hornedBeastValues.map((beast, idx) => (
+              <HornedBeast key={idx} title={beast.title} image_url={beast.image_url} description={beast.description}/>
+          ))}
+      </section>
     )
   }
 }
--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import Card from 'react-bootstrap/Card';
 
 class HornedBeast extends React.Component {
   constructor(props) {
@@ -17,16 +18,14 @@ class HornedBeast extends React.Component {
 
   render() {
     return (
-      <div>
-        <h2>{this.props.title}</h2>
-        <img src={this.props.image_url} alt={this.props.title} title={this.props.title} />
-        {/* <button  onClick={this.handleClick}>Favorite Me!</button> */}
-        <div>
-        <FontAwesomeIcon icon={faHeart} onClick={this.handleClick}  />
-        <p>{this.state.count}</p>
-        </div>
-        <p>{this.props.description}</p>
-      </div>
+      <Card>
+        <Card.Img src={this.props.image_url} alt={this.props.title} title={this.props.title} />
+        <Card.Body>
+        <Card.Title>{this.props.title}</Card.Title>
+       <Card.Text>  <FontAwesomeIcon icon={faHeart} onClick={this.handleClick}  /><span>{this.state.count}</span> </Card.Text>
+       <Card.Text>{this.props.description}</Card.Text>
+        </Card.Body>
+      </Card>
     )
   }
 }
