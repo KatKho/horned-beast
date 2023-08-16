515cd49ebec70a96afde5e28bebf2bb8202bbf6c
Ekaterina Khoroshilova <ek.khorosh@gmail.com>
2023-08-16 14:49:16 -0700

    completes lab 03

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,42 @@
 import './App.css'
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Gallery from './components/Gallery';
 import Footer from './components/Footer';
+import SelectedBeast from './components/SelectedBeast';
+import hornedBeastValues from './assets/data.json';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      selectedBeast: null,
+    };
+  }
+
+  openModal = (title, image_url, description) => {
+    const selectedBeast = { title, image_url, description };
+    this.setState({ showModal: true, selectedBeast });
+  };
+
+  closeModal = () => {
+    this.setState({ showModal: false, selectedBeast: null });
+  }
+render() {
 
   return (
     <>
       <div>
       <Header />
-      <Gallery />
+      <Gallery beastData={hornedBeastValues} openModal={this.openModal} />
+      <SelectedBeast showModal={this.state.showModal} closeModal={this.closeModal} selectedBeast={this.state.selectedBeast} />
       <Footer />
       </div>
     </>
   )
+}
 }
 
 export default App
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-import hornedBeastValues from '../assets/data.json';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 class Gallery extends React.Component {
   render() {
+    const { beastData, openModal } = this.props;
+
     return (
       <Container>
         <Row>
-          {hornedBeastValues.map((beast, idx) => (
+          {beastData.map((beast, idx) => (
             <Col key={idx} xs={12} sm={6} md={4} lg={3}>
               <HornedBeast
                 title={beast.title}
                 image_url={beast.image_url}
                 description={beast.description}
+                openModal={openModal}
               />
             </Col>
           ))}
