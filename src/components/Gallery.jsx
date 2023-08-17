d05150a3e73119fc8a4b8a159100edb1ca0ffbd9
Ekaterina Khoroshilova <ek.khorosh@gmail.com>
2023-08-17 14:57:04 -0700

    completes lab-04

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -8,23 +8,40 @@ class Gallery extends React.Component {
   render() {
     const { beastData, openModal } = this.props;
 
+    
     return (
       <Container>
         <Row>
-          {beastData.map((beast, idx) => (
-            <Col key={idx} xs={12} sm={6} md={4} lg={3}>
-              <HornedBeast
-                title={beast.title}
-                image_url={beast.image_url}
-                description={beast.description}
-                openModal={openModal}
-              />
-            </Col>
-          ))}
+          {beastData.map((beast, idx) => {
+            if (beastData.length === 2) {
+              return (
+                <Col key={idx} xs={12} md={6} className="mb-4">
+                  <HornedBeast
+                    title={beast.title}
+                    image_url={beast.image_url}
+                    description={beast.description}
+                    openModal={openModal}
+                  />
+                </Col>
+              );
+            } else {
+              return (
+                <Col key={idx} xs={12} sm={6} md={4} lg={3} className="mb-4">
+                  <HornedBeast
+                    title={beast.title}
+                    image_url={beast.image_url}
+                    description={beast.description}
+                    openModal={openModal}
+                  />
+                </Col>
+              );
+            }
+          })}
         </Row>
       </Container>
     );
   }
 }
+
 
 export default Gallery;
